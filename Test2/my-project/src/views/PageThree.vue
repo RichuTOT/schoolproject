b5e6a16234b03f708d4dcf2d01391a6892a26012
RichuTOT <lucaszhuo123@gmail.com>
2024-07-05 16:34:34 +0800

    个人中心，社团搜索，首页

---
+++ Test2/my-project/src/views/PageThree.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <h1>Page Three</h1>
+      <el-table :data="tableData">
+        <el-table-column prop="date" label="Date" width="180"></el-table-column>
+        <el-table-column prop="name" label="Name" width="180"></el-table-column>
+        <el-table-column prop="address" label="Address"></el-table-column>
+      </el-table>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        tableData: [
+          { date: '2016-05-02', name: 'Tom', address: 'No. 189, Grove St, Los Angeles' },
+          // 添加更多数据
+        ]
+      };
+    }
+  };
+  </script>
+  
